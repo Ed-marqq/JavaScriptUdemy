5c58e8fc6f183a825f3a4a2f04d6ac3005eaaead
Ed.marqq <eduardomarques.principal@gmail.com>
2023-10-04 21:44:14 -0300

    estudo até aula 08

---
+++ aula08/indexTexte.js
@@ -0,0 +1,19 @@
+const nome = 'Eduardo'
+const sobrenome = 'Marques'
+const idade = '26'
+const peso = '60'
+const altura = 1.63
+let anoNascimento 
+let imc //peso/(altura*altura)
+
+imc = peso / (altura*altura)
+anoNascimento = 2023 - idade
+
+console.log(`${nome} ${sobrenome}, nascido em ${anoNascimento}, tem ${idade} anos, ${altura} de altura, pesa ${peso} Kg, e seu IMC é de ${imc}`)
+
+
+
+
+
+
+
