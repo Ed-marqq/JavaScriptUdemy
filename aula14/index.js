8ce4724618b03bcebdf502e60414bf17c01c0b7c
Ed.marqq <eduardomarques.principal@gmail.com>
2023-10-10 22:16:03 -0300

    created aula14

---
+++ aula14/index.js
@@ -0,0 +1,5 @@
+let num1 = 22.5474786
+
+//console.log(num1.toString(2)) > Representação binária da variável
+
+//console.log(num1.toFixed(1)) >  Reduzir casas decimais > número entre parênteses representa a quantidade de casas decimais
