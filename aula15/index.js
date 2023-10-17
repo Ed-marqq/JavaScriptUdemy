968ba83b3d2dfd0267ef97d8ff1df6a75802cc58
Ed.marqq <eduardomarques.principal@gmail.com>
2023-10-16 21:38:10 -0300

    ex criados até o 16

---
+++ aula15/index.js
@@ -0,0 +1,18 @@
+let num1 = 9.214314
+//***Arredonda para baixo*** 
+//let num2 = Math.floor(num1)
+
+//***Arredonda para cima
+//let num2 = Math.ceil(num1)
+
+//*** Arredonda para o mais próximo
+//let num2 = Math.round(num1)
+
+//*** Mostra o maior / menor número da sequência
+//console.log  (Math.max(1,2,5,85,94565,23425,698797,5645634,343))
+//console.log  (Math.min(1,2,5,85,94565,23425,698797,5645634,343))
+
+// Gerar números aleatórios
+//console.log(Math.random()) 
+// para gerar números entre 0 e 10
+//console.log(Math.random()*(10-5)+5) 
