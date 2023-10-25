752e4fbbf4bb5512ae35888954e0bcfe51699cca
Ed.marqq <eduardomarques.principal@gmail.com>
2023-10-25 06:31:30 -0300

    Update valores_primitivos_e_por_referencia.js

--- aula19/valores_primitivos_e_por_referencia.js
+++ aula19/valores_primitivos_e_por_referencia.js
@@ -9,3 +9,21 @@
 
 
 */
+
+
+//## Primitivos (imutáveis)
+/*let a = 2
+let b = a
+console.log(a)
+console.log(b)
+console.log(b)
+a= 3
+console.log(a, b)*/
+
+//## Por referência (mutáveis)
+
+let a = [1,2,3]
+let b = a
+console.log(a,b)
+a.push(4)
+console.log(a,b)
