c31fe1e4f7f50d4dba5ddc04911efdedf1b5fd1c
Ed.marqq <eduardomarques.principal@gmail.com>
2023-11-06 21:27:07 -0300

    created aula 21 e 22

---
+++ aula21/operadores_comparacao.js
@@ -0,0 +1,17 @@
+
+
+/*Operadores de comparação
+
+> Maior que
+>= Maior ou igual
+< Menor que
+<= Menor ou igual
+= Atribuição
+== Igualdade (valor)
+=== Igualdade restrita (valor e tipo)
+!= Diferente (valor)
+!== Diferente estrito (valor e tipo)
+
+
+
+*/
---
+++ aula22/operadores_logicos.js
@@ -0,0 +1,8 @@
+/*
+Operadores lógicos
+
+&& = and = E > V+V = V 
+|| = or = ou > F+F = F
+! = not = não
+
+*/
